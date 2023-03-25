eca66337fa9608714e43fc4f1021c5ae8660b589
Rui Chen <rui@chenrui.dev>
2023-03-25 12:36:46 -0700

    update config
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,8 +6,18 @@ module.exports = {
   solidity: "0.8.17",
 
   networks: {
+    OKCMainnet: {
+      url: process.env.OKCMainnet,
+      blockGasLimit: 10000000,
+      gas: 10000000,
+      chainId: 66,
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      accounts: [process.env.OKCMainnetPrivateKey],
+      allowUnlimitedContractSize: true,
+    },
     OKCTestnet: {
-      url: "https://exchaintestrpc.okex.org",
+      url: process.env.OKCTestnetNode,
       // blockGasLimit: 10000000,
       // gas: 10000000,
       chainId: 65,
