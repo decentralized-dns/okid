ff279fc86dc9523d2ef8d0d9451ac242ccd046cb
Rui Chen <rui@chenrui.dev>
2023-03-25 14:16:08 -0700

    hardhat: deploy to testnet (#4)
    * hardhat: deploy to testnet
    
    Signed-off-by: Rui Chen <rui@chenrui.dev>

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+
+  networks: {
+    OKCMainnet: {
+      url: process.env.OKCMainnet,
+      blockGasLimit: 10000000,
+      gas: 10000000,
+      chainId: 66,
+      throwOnTransactionFailures: true,
+      throwOnCallFailures: true,
+      accounts: [process.env.OKCMainnetPrivateKey],
+      allowUnlimitedContractSize: true,
+    },
+    OKCTestnet: {
+      url: process.env.OKCTestnetNode,
+      // blockGasLimit: 10000000,
+      // gas: 10000000,
+      chainId: 65,
+      // throwOnTransactionFailures: true,
+      // throwOnCallFailures: true,
+      accounts: [process.env.OKCTestnetPrivateKey],
+      // allowUnlimitedContractSize: true,
+    },
+  },
 };
